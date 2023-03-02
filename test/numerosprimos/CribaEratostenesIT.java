b3b2e0a01288d752dc780c2ff1bbfafc07bd5073
Josem27 <josem.cruzregalado@gmail,com>
2023-03-02 16:24:10 +0100

    [JUNIT] Primer Commit Junit con pruebas con los diferentes metodos

--- test/numerosprimos/CribaEratostenesIT.java
+++ test/numerosprimos/CribaEratostenesIT.java
@@ -4,10 +4,6 @@
  */
 package numerosprimos;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -16,38 +12,41 @@ import static org.junit.Assert.*;
  * @author josem
  */
 public class CribaEratostenesIT {
-    
-    public CribaEratostenesIT() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
     /**
      * Test of generarPrimos method, of class CribaEratostenes.
      */
+
     @Test
-    public void testGenerarPrimos() {
-        System.out.println("generarPrimos");
+    public void testGeneraPrimosConCero() {
         int max = 0;
-        int[] expResult = null;
+        int expResult = 0;
+        int[] result = CribaEratostenes.generarPrimos(max);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testGeneraPrimosConDos() {
+        int max = 2;
+        int expResult = 2;
+        int[] result = CribaEratostenes.generarPrimos(max);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testGeneraPrimosConTres() {
+        int max = 3;
+        int[] expResult = {2, 3};
         int[] result = CribaEratostenes.generarPrimos(max);
         assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-    
+
+    @Test
+    public void testGeneraPrimosConCien() {
+        int max = 100;
+        int[] expResult = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+        int[] result = CribaEratostenes.generarPrimos(max);
+        assertArrayEquals(expResult, result);
+    }
+
 }
